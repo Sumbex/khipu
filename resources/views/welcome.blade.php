97ee27aeaa1ca4f247558a9d5fbc45f09538304c
David Luna <david.luna.cuevas@gmail.com>
2017-08-10 16:25:19 -0400

    View Prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,88 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+  <script src="chrome-extension://obkfjhifkbhimlocpddgamonjihinpak/script/safebrowse.js"></script>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>Khipu Beta</title>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    
+      <div class="container">
+        <nav class="navbar navbar-inverse navbar-static-top">
+        
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">Khipu</a>
             </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Inicio</a></li>
+                <li><a href="#about">Nosotros</a></li>
+                <li><a href="#contact">Contacto</a></li>
+              </ul>
+                </li>
+              </ul>
+            </div>
+      
+        </nav>
+
+      </div>
+ 
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="http://thumbs.ebaystatic.com/d/l140/m/mkZ-Kw53N9U4b9Q-zk0tBXw.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Goku Niño</h2>
+          <p>Figura edicion de Dragon Ball.</p>
+          <p><a class="btn btn-default" href="#" role="button">Ver Detalles »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="https://i.ebayimg.com/thumbs/images/g/o3sAAOSw2gxYpYxc/s-l140.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Goku SS2</h2>
+          <p>Figura edicion de Dragon Ball Z.</p>
+          <p><a class="btn btn-default" href="#" role="button">Ver Detalles »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="http://www.ciudadmangacr.com/image/cache/data/DBZ/31500%20-%20045557343996%20-%20big%20budoukai%20SSJ3%20Goku-140x140.jpeg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Goku SS3</h2>
+          <p>Figura edicion de Dragon Ball Z.</p>
+          <p><a class="btn btn-default" href="#" role="button">Ver Detalles »</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Coleccion Saga de Black. <span class="text-muted">Edicion Limitada</span></h2>
+          <p class="lead">Lo ultimo en calidad de figuras de accion, compra 4 figuras de accion de DBS edicion limitada</p>
+          <p><a class="btn btn-Primary" href="/pago" role="button">Comprar Ahora</a></p>
         </div>
-    </body>
-</html>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="https://ae01.alicdn.com/kf/HTB167naPVXXXXXrXFXXq6xXFXXXM/Dragon-Ball-Z-SUPER-Power-66-font-b-Action-b-font-font-b-Figure-b-font.jpg" data-holder-rendered="true">
+        </div>
+      </div>
+      <!-- /END THE FEATURETTES -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ 
